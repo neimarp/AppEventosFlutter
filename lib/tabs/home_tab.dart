beaac595f9ae208a96a7f4eee2ef0efbe44bb233
Douglas Sutil <douglassutil@gmail.com>
2019-04-28 21:06:35 -0300

    listview eventos

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:loja/tiles/icon_tiles.dart';
 
 class HomeTab extends StatefulWidget {
   HomeTab();
@@ -7,7 +9,6 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
-    
   Firestore _firestore = Firestore.instance;
   List<DocumentSnapshot> _eventos = [];
   bool _loadingEventos = true;
@@ -54,17 +55,17 @@ class _HomeTabState extends State<HomeTab> {
 
     QuerySnapshot querySnapshot = await q.getDocuments();
 
-    if (querySnapshot.documents.length < _itensPorPagina ) {
+    if (querySnapshot.documents.length < _itensPorPagina) {
       _maisEventosExistentes = false;
     }
 
-    if (querySnapshot.documents.length > 0 ) {
-      _lastDocument = querySnapshot.documents[querySnapshot.documents.length - 1];
+    if (querySnapshot.documents.length > 0) {
+      _lastDocument =
+          querySnapshot.documents[querySnapshot.documents.length - 1];
     }
     _eventos.addAll(querySnapshot.documents);
 
-    setState(() {
-    });
+    setState(() {});
 
     _gettingMaisEventos = false;
   }
@@ -92,25 +93,171 @@ class _HomeTabState extends State<HomeTab> {
         title: Text("Eventos"),
         centerTitle: true,
       ),
-      drawer: Drawer(child: Container(),),
+      drawer: Drawer(
+        child: Container(),
+      ),
       body: _loadingEventos == true
           ? Center(child: CircularProgressIndicator())
           : Container(
               child: _eventos.length == 0
+                  // Se não tiver eventos, aparece somente um texto no centro
                   ? Center(child: Text("Sem eventos no momento!"))
+
+                  // Se tiver eventos, aparece a listagem "ListView"
                   : ListView.builder(
                       controller: _scrollController,
                       itemCount: _eventos.length,
                       itemBuilder: (BuildContext context, int index) {
-                        return ListTile(
-                          leading: CircleAvatar(
-                            child: ClipOval(
-                                child: _eventos[index].data["imagem"] != null 
-                                ? Image.network(_eventos[index].data["imagem"])
-                                : Container()
-                                ),
+
+                        // Padding para dar espaçamentos nas bordas
+                        return Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.of(context).size.width * 0.010,
+                              vertical:
+                                  MediaQuery.of(context).size.width * 0.010),
+                          
+                          // Container com as informações
+                          child: Container(
+                            child: Material(
+                              color: Colors.white,
+                              elevation: 7.0,
+                              borderRadius: BorderRadius.circular(5.0),
+                              
+                              child: Row(
+                                children: <Widget>[
+
+                                  // Box com a imagem
+                                  Container(
+                                    height:
+                                        MediaQuery.of(context).size.width * 0.28,
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.38,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.horizontal(left: Radius.circular(5.0)),
+                                      child: _eventos[index].data["imagem"] != null
+                                              ? Image.network(
+                                                  _eventos[index].data["imagem"],
+                                                  fit: BoxFit.cover,
+                                                  alignment: Alignment.topLeft,
+                                                )
+                                              : Container(),
+                                    ),
+                                  ),
+
+                                  // Box com as informações
+                                  Container(
+                                    height:
+                                        MediaQuery.of(context).size.width * 0.28,
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.59,
+                                 
+                                    padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.03),
+
+                                    child: Column(
+                                      children: <Widget>[
+
+                                        // Título do Evento
+                                        Container(
+                                          alignment: Alignment.bottomLeft,
+                                          height: MediaQuery.of(context).size.width * 0.075,                                          
+                                          child: Text(_eventos[index].data["nome"],
+                                            maxLines: 1, 
+                                            overflow: TextOverflow.ellipsis,
+                                            style:TextStyle(
+                                                    color: Colors.green,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: MediaQuery.of(context).size.width * 0.06
+                                                  )
+                                          ),
+                                        ),
+
+                                        // Descrição
+                                        Container(
+                                          alignment: Alignment.centerLeft,
+                                          height: MediaQuery.of(context).size.width * 0.115,                                          
+                                          child: Text(_eventos[index].data["descricao"],
+                                            maxLines: 2, 
+                                            overflow: TextOverflow.ellipsis,
+                                            style:TextStyle(
+                                                    color: Colors.green,
+                                                    fontWeight: FontWeight.bold,
+                                                    fontSize: MediaQuery.of(context).size.width * 0.032
+                                                  )
+                                          ),
+                                        ),
+                                        
+                                        // Linha de Icones de observação
+                                        Container(
+                                          height: MediaQuery.of(context).size.width * 0.09,
+                                          
+                                          child: Row(
+                                            children: <Widget>[
+
+                                              // ícone de horario de evento
+                                              _eventos[index].data["hora"] == "" 
+                                                ? Container()
+                                                : IconTile(
+                                                    icon:Icons.watch_later,
+                                                    text: _eventos[index].data["hora"],
+                                                  )
+                                              ,
+                                              
+                                              // ícone de sexo do evento
+                                              _eventos[index].data["sexo"] == "" 
+                                                ? Container()
+                                                : _eventos[index].data["sexo"] == "M"
+                                                  ? IconTile(
+                                                      icon: FontAwesomeIcons.female,
+                                                      text: _eventos[index].data["sexo"]
+                                                    )
+                                                  : _eventos[index].data["sexo"] == "H"
+                                                    ? IconTile(
+                                                        icon: FontAwesomeIcons.male,
+                                                        text: _eventos[index].data["sexo"]
+                                                      )
+                                                    : Container()
+                                              ,
+
+                                              // ícone de evento pago
+                                              _eventos[index].data["pago"] == "" 
+                                                ? Container()
+                                                : _eventos[index].data["pago"] == "s"
+                                                  ? IconTile(
+                                                     icon:Icons.monetization_on,
+                                                     text: "Pago",
+                                                  )
+                                                  : _eventos[index].data["pago"] == "n"
+                                                    ? IconTile(
+                                                      icon:Icons.money_off,
+                                                      text: "Free",
+                                                    )
+                                                    : Container()
+                                              ,
+
+                                              // ícone de estacionamento
+                                              _eventos[index].data["estacionamento"] == "" 
+                                                ? Container()
+                                                : _eventos[index].data["estacionamento"] == "s" 
+                                                  ? IconTile(
+                                                      icon:Icons.local_parking,
+                                                      text: "Est",
+                                                    )
+                                                  : Container()
+                                              ,
+
+                                              
+
+                                            ],
+                                          )                                          
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
-                          title: Text(_eventos[index].data["nome"]),
                         );
                       },
                     ),
